2209f8556506a26225609c8fc5d5c6da3a709b16
yam-1111 <achinay0341ant@student.fatima.edu.ph>
2024-06-03 08:47:16 +0800

    added saving info on the personal information

--- src/static/js/formPage.js
+++ src/static/js/formPage.js
@@ -1,19 +1,50 @@
-console.log("i am executed")
+console.log("form loaded")
 window.counter = 0;
 const pages = ["personal_info", "children", "education"];
+var formData = {}
 
 $(document).ready(function () {
     // Function to load content
     window.loadPage = (page) => {
+
+        $("input, select, textarea").each(function () {
+            if ($(this).is(":radio")) {
+                if ($(this).is(":checked")) {
+                    formData[$(this).attr('name')] = $(this).val();
+                }
+            } else {
+                formData[$(this).attr('name')] = $(this).val();
+            }
+        });
+
         $("#parentContainer").fadeOut(100, function () {
             $(this).load("/static/html/form/" + page + ".html", function () {
                 $(this).fadeIn(150);
+                // Repopulate the form data
+                for (let name in formData) {
+                    let element = $(`[name="${name}"]`);
+                    if (element.is(":radio")) {
+                        element.filter(`[value="${formData[name]}"]`).prop("checked", true);
+                    } else {
+                        element.val(formData[name]);
+                    }
+                }
                 // Update navigation buttons visibility
                 if (counter == 0) {
                     $("#prevButton").hide();
                 } else {
                     $("#prevButton").show();
                 }
+
+                if (counter == 2) {
+                    $("#nextButton").hide();
+                    $("#buttonContainer").append('<button type="submit" class="btn btn-success" id="submitForm">Submit Form</button>');
+                } else {
+                    $("#nextButton").show();
+                    // Ensure the submit button is removed if it exists and the counter is not 2
+                    $("#submitForm").remove();
+                }
+
             });
         });
     }
